9e07d70d4afcbb4d004efb099c71df0692e7c028
DannyBoyNYC <daniel.deverell@gmail.com>
2016-10-25 12:02:49 -0400

    Added image switcher

--- app/recipe-detail/recipe-detail.component.js
+++ app/recipe-detail/recipe-detail.component.js
@@ -4,8 +4,13 @@ angular.module('recipeDetail').component('recipeDetail', {
         function RecipeDetailController($http, $routeParams) {
             var self = this;
 
+            self.setImage = function setImage(imageUrl) {
+                self.mainImageUrl = imageUrl;
+            };
+
             $http.get('data/' + $routeParams.recipeId + '.json').then(function (response) {
                 self.recipe = response.data;
+                self.setImage(self.recipe.images[0]);
             });
         }
     ]
